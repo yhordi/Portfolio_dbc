d928f2452edba176bb375206e163fef431120dfe
Jordan Kamin <Jordan@Jordans-MacBook-Air.local>
2014-07-09 11:10:02 -0700

    finishing touches on animations

--- javascripts/view.js
+++ javascripts/view.js
@@ -1,15 +1,18 @@
 var View = function() {
+  this.topHit = 0
 }
 
 View.prototype = {
   fix: function () {
     if ($(window).scrollTop() >= 296) {
+      this.view.topHit = 1
       this.view.addFixed()
       this.view.addSlideIn()
       this.view.socialVisible()
       } else {
-        this.view.removeFixed()
-        this.view.removeSlideIn()
+      this.view.removeFixed()
+      this.view.removeSocialVisible()
+      this.view.removeSlideIn()
       }
   },
   addFixed: function () {
@@ -26,5 +29,8 @@ View.prototype = {
   },
   socialVisible: function() {
     $(".social").addClass("visible")
-  }
+  },
+  removeSocialVisible: function() {
+    $(".social").removeClass("visible")
+  },
 }
