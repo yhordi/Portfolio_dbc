b5e5e749f1b3a531eb5b0ca329bcba570f9247de
Jordan Kamin <Jordan@Jordans-MacBook-Air.local>
2014-07-18 17:11:10 -0700

    more js cleanup

--- javascripts/view.js
+++ javascripts/view.js
@@ -1,11 +1,9 @@
 var View = function() {
-  this.topHit = 0
 }
 
 View.prototype = {
   fix: function () {
     if ($(window).scrollTop() >= 296) {
-      this.view.topHit = 1
       this.view.addFixed()
       this.view.addSlideIn()
       this.view.socialVisible()
