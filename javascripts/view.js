ace4bbc68af9633d215e1e401b42b9968d204560
Jordan Kamin <Jordan@Jordans-MacBook-Air.local>
2014-07-22 15:05:51 -0700

    replaced dummy text with actual description, shrunk some vertical margins, removed text fixing

--- javascripts/view.js
+++ javascripts/view.js
@@ -7,14 +7,44 @@ View.prototype = {
       this.view.addFixed()
       this.view.addSlideIn()
       this.view.socialVisible()
-      } else {
+      // this.view.addZIndex()
+      this.view.fadeArrowOut()
+    } else {
       this.view.removeFixed()
       this.view.removeSocialVisible()
       this.view.removeSlideIn()
       }
+    // if ($(window).scrollTop() >= 811) {
+    //   this.view.addFixedToDbc()
+    // } else {
+    //   this.view.removeFixedFromDbc()
+    // }
+    // if ($(window).scrollTop() >= 2000 ) {
+    //   this.view.dbcHide()
+    // } else {
+    //   this.view.dbcShow()
+    // }
   },
   addFixed: function () {
     $(".intro").addClass("fixed");
+  },
+  addZIndex: function () {
+    $(".intro").addClass("zIndex")
+  },
+  addDisplayInline: function() {
+    $(".intro").addClass("displayInline")
+  },
+  addFixedToDbc: function () {
+    $(".dbcText").addClass("fixed");
+  },
+  removeFixedFromDbc: function () {
+    $(".dbcText").removeClass("fixed");
+  },
+  dbcHide: function() {
+    $(".dbcText").hide()
+  },
+  dbcShow: function() {
+    $(".dbcText").show()
   },
   addSlideIn: function () {
     $(".intro").addClass("slideIn")
@@ -31,4 +61,7 @@ View.prototype = {
   removeSocialVisible: function() {
     $(".social").removeClass("visible")
   },
+  fadeArrowOut: function() {
+    $(".arrow").addClass("arrowFadeOut")
+  }
 }
